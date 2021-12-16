d258f205513d03b6209d0bca59afb41c905aa107
Ahmed Saber <ahmed0saber33@gmail.com>
2021-12-16 21:17:05 +0200

    Add files via upload

--- portfolio/js/functionality.js
+++ portfolio/js/functionality.js
@@ -6,7 +6,7 @@ fetch('data.json')
             var projects = data[0];
             for(var i=0; i<projects.length; i++){
                 document.getElementById("all_projects").innerHTML+=`<div class="col-sm-12 col-md-6 col-lg-4 container-md p-1">
-                    <div class="p-4 border bg-light text-dark rounded project" style="background-image: url('${projects[i].img}')">
+                    <div class="p-4 border bg-light text-dark rounded project d-flex align-items-end" style="background-image: url('${projects[i].img}')">
                         <div class="container-fluid bg-light p-2 rounded border hidden">
                             <h1>${projects[i].title}</h1>
                             <p>${projects[i].description}</p>
